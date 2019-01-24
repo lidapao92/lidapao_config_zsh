bb98138e38df403d95dac69b76a6a235ed515fd0
lidapao <fullstack.lx@gmail.com>
2019-01-24 17:50:47 +0800

    change install

--- install.sh
+++ install.sh
@@ -70,4 +70,19 @@ else
 fi
 ~/.fzf/install --all
 
+  # If this user's login shell is not already "zsh", attempt to switch.
+TEST_CURRENT_SHELL=$(basename "$SHELL")
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    # If this platform provides a "chsh" command (not Cygwin), do it, man!
+    if hash chsh >/dev/null 2>&1; then
+        printf "Time to change your default shell to zsh!\n"
+        chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # Else, suggest the user do so manually.
+    else
+        printf "I can't change your shell automatically because this system does not have chsh.\n"
+        printf "Please manually change your default shell to zsh!\n"
+    fi
+fi
+
+rm ~/.zcompdump*
 env zsh -l
