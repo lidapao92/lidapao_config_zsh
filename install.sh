fad403a6dc61053b3ae2327533ba350121db2212
lidapao <fullstack.lx@gmail.com>
2019-10-20 21:20:07 +0800

    fix zsh-autosuggestion -> zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ fi
 
 printf "Install oh-my-zsh plugins...\n";
 if ! [ -d "$ZSH_CUSTOM_PLUGINS/zsh-autosuggestion" ]; then
-    env git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM_PLUGINS/zsh-autosuggestion" || {
+    env git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM_PLUGINS/zsh-autosuggestions" || {
         printf "Error: git clone of zsh-autosuggestion repo failed\n"
         exit 1
     }
